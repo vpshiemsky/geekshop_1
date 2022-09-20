850951623591b0aa6bf85077e4b56aae807d64ae
vpshiemsky <vpshiemsky@mail.ru>
2022-09-20 09:29:28 +0300

    -

--- adminapp/templatetags/my_tags.py
+++ adminapp/templatetags/my_tags.py
@@ -1,5 +1,5 @@
-from django.conf import settings
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -7,14 +7,14 @@ register = template.Library()
 @register.filter(name='media_for_products')
 def media_for_products(img_path):
     if not img_path:
-        img_path =  'products/default.jpg'
+        img_path =  'products/default.png'
 
         return f'{settings.MEDIA_URL}{img_path}'
 
 
 def media_for_users(img_path):
     if not img_path:
-        img_path =  'users_avatars/default.jpg'
+        img_path =  'users_avatars/default.png'
 
         return f'{settings.MEDIA_URL}{img_path}'
 
